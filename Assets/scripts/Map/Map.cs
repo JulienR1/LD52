ba4364accd3a5ecfe94f0d92109d428bf7754e0e
Samuel Bouliane <samuelbouliane@gmail.com>
2023-01-08 11:35:15 -0500

    Map generation complete

--- Assets/scripts/Map/Map.cs
+++ Assets/scripts/Map/Map.cs
@@ -7,74 +7,97 @@ public class Map : MonoBehaviour
     public int levelWidth;
     public int levelHeight;
 
+    public int mapScale = 100;
     private float tileSize = 1.0f;
 
     public GameObject tilePrefab;
 
     public Sprite[] groundSprites;
+    public Sprite[] clockwiseCornerSprites;
+    public Sprite edgeHorizontalSprite;
+    public Sprite edgeVerticalSprite;
 
     public GameObject[,] tiles;
 
-    public Sprite topBottomSprite;
-    public Sprite leftRightSprite;
 
     private void Start() {
         CreateMap();    
     }
 
-    void CreateMap()
+    private void CreateMap()
     {
-        tiles = new GameObject[levelWidth, levelHeight];
+        tiles = new GameObject[mapScale, mapScale];
         
+        // Name the tiles with their coordinates and make sure that 0,0 is in the center centered tile
+        for (int x = 0; x < mapScale; x++)
+        {
+            for (int y = 0; y < mapScale; y++)
+            {
+                Vector2 tilePosition = new Vector2(x * tileSize, y * tileSize);
+                GameObject tile = GameObject.Instantiate(tilePrefab, tilePosition, Quaternion.identity);
 
+                tile.GetComponent<SpriteRenderer>().sprite = groundSprites[Random.Range(0, groundSprites.Length)];
+                            
+                tile.transform.parent = GameObject.Find("World").transform;
+                tile.name = "World " + x + "_" + y + "";
+                tiles[x, y] = tile;
+            }
+        }
+
+        GenerateLevel(tiles);
+    }
+
+
+    private void GenerateLevel(GameObject[,] tiles){
+        int startX = mapScale/2 - levelWidth/2;
+        int startY = mapScale/2 - levelHeight/2;
+        
+        // Recentre le niveau
+        for (int x = startX; x < startX + levelWidth + 2; x++)
+        {
+            for (int y = startY; y < startY + levelHeight + 2; y++)
+            {
+                GameObject tile = tiles[x, y];
+
+                GameObject edgeTile = GameObject.Instantiate(tile, tile.transform.position, Quaternion.identity);
+                edgeTile.transform.parent = GameObject.Find("Level Edges").transform;
+                edgeTile.name = "Edge " + x + "_" + y + "";
+                edgeTile.GetComponent<SpriteRenderer>().sortingOrder = 1;
+
+                // Génère les bordures du niveau et détruit les tuiles dupliqués à l'intérieur du niveau 
+                if (x == startX || x == startX + levelWidth + 1)
+                {
+                    edgeTile.GetComponent<SpriteRenderer>().sprite = edgeVerticalSprite;
+                }
+                else if (y == startY || y == startY + levelHeight + 1)
+                {
+                    edgeTile.GetComponent<SpriteRenderer>().sprite = edgeHorizontalSprite;
+                }
+                else{
+                    Destroy(edgeTile);
+                }
+
+                // Change le sprite des coins pour le sprite de coin respectif
+                if (x == startX && y == startY){
+                    edgeTile.GetComponent<SpriteRenderer>().sprite = clockwiseCornerSprites[3];
+                }else if(x == startX + levelWidth + 1 && y == startY + levelHeight + 1){
+                    edgeTile.GetComponent<SpriteRenderer>().sprite = clockwiseCornerSprites[1];
+                }else if(x == startX + levelWidth + 1 && y == startY){
+                    edgeTile.GetComponent<SpriteRenderer>().sprite = clockwiseCornerSprites[2];
+                }else if(x == startX && y == startY + levelHeight + 1){
+                    edgeTile.GetComponent<SpriteRenderer>().sprite = clockwiseCornerSprites[0];
+                }
+            }
+        }
+
+        // Renomme les tuiles du (niveau seulement) sur un plan cartésien
         for (int x = 0; x < levelWidth; x++)
         {
             for (int y = 0; y < levelHeight; y++)
             {
-                GameObject tile = GameObject.Instantiate(tilePrefab, new Vector2(x * tileSize, y * tileSize), Quaternion.identity);
-                
-                if (x == 0 || x == levelWidth + 1)
-                {
-                    // horizontal edges
-                    if (y == 0)
-                    {
-                        // top-left corner
-                        tile.GetComponent<SpriteRenderer>().sprite = groundSprites[0];
-                    }
-                    else if (y == levelHeight + 1)
-                    {
-                        // bottom-left corner
-                        tile.GetComponent<SpriteRenderer>().sprite = groundSprites[2];
-                    }
-                    else
-                    {
-                        tile.GetComponent<SpriteRenderer>().sprite = topBottomSprite;
-                    }
-                }
-                else if (y == 0 || y == levelHeight + 1)
-                {
-                    // vertical edges
-                    if (x == 0)
-                    {
-                        // top-right corner
-                        tile.GetComponent<SpriteRenderer>().sprite = groundSprites[0];
-                    }
-                    else if (x == levelWidth + 1)
-                    {
-                        // bottom-right corner
-                        tile.GetComponent<SpriteRenderer>().sprite = groundSprites[1];
-                    }
-                    else
-                    {
-                        tile.GetComponent<SpriteRenderer>().sprite = leftRightSprite;
-                    }
-                }
-                else
-                {
-                    tile.GetComponent<SpriteRenderer>().sprite = groundSprites[Random.Range(0, groundSprites.Length)];
-                }
-                
-                tiles[x, y] = tile;
+                GameObject tile = tiles[x + startX + 1, y + startY + 1];
+                tile.transform.parent = GameObject.Find("Level").transform;
+                tile.name = "(" + (x - levelWidth/2) + ", " + (y - levelHeight/2) + ")";
             }
         }
     }
